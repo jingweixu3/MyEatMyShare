a0cd7c786f4b5255557498006d05ee3e624302b7
sirui <siruipan@usc.edu>
2021-05-09 21:02:11 -0700

    change ui in usergrid

--- client/src/components/ShowFollower.js
+++ client/src/components/ShowFollower.js
@@ -10,20 +10,18 @@ const ShowFollower = ({foundUser, userInfo, follower}) =>  {
 
     return (
         <div className="container">
-    <nav aria-label="breadcrumb" className="main-breadcrumb">
+    {/* <nav aria-label="breadcrumb" className="main-breadcrumb">
         <ol className="breadcrumb">
         <li className="breadcrumb-item"><a href="/">Home</a></li>
         <li className="breadcrumb-item active" aria-current="page">Search Result</li>
         </ol>
-    </nav>
-    <div className="row ng-scope">
-        <div className="col-md-9 col-md-pull-3">
+    </nav> */}
+    <div>
+        <div>
             {/* <p className="search-results-count">About {foundUser.length} results</p> */}
-            <section className="search-result-item">
                 {foundUser && follower && follower.map((info) => (
                     <UserGrid user = {info} userInfo={userInfo} isProfile = {true}/>
                 ))}
-            </section>
         </div>
     </div>
     </div>
--- client/src/components/UserGrid.js
+++ client/src/components/UserGrid.js
@@ -1,11 +1,36 @@
 import React,{ useState, useEffect} from "react";
-import Navbar from "./Navbar/Navbar";
 import Axios from "axios";
-import axios from "axios";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 800,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '128px',
+  },
+}));
 
 const UserGrid = ({user, userInfo,isProfile}) =>  {
-    //user is found user and userInfo is the current login user's info
-    const [follow, setFollow] = useState(false);
+  const classes = useStyles();
+  const [follow, setFollow] = useState(false);
     const [self, setSelf] = useState(false);
 
     // console.log("userrrrindoooo", userInfo);
@@ -54,30 +79,50 @@ const UserGrid = ({user, userInfo,isProfile}) =>  {
           });
         }
       };
-    // console.log("foll", follow);
-    
-    return(
-        <div>
-           {user && user.avatar === "" && <a className="image-link" href={`/profile/${user.id}`}><img className="image" style={{width:"30%", height:"20vw"}} src="https://bootdey.com/img/Content/avatar/avatar1.png" /></a>}
-           {user && user.avatar !== "" && <a className="image-link" href={`/profile/${user.id}`}><img className="image" style={{width:"30%", height:"20vw"}} src={user.avatar}/></a>}
-            <div className="search-result-item-body">
-                <div className="row">
-                    <div className="col-sm-9">
-                        <h4 className="search-result-item-heading"><a href={`/profile/${user.id}`}>{user.firstName} {user.lastName}</a></h4>
-                        <p className="info">{user.location}</p>
-                        <p className="description">{user.note}</p>
-                    </div>
-                   <div className="col-sm-3 text-align-center">
-                        {/* {!follow && !self && <button className="btn btn-primary btn-info btn-sm" onClick={handleClick}>Follow</button>}
-                        {follow && !self && <button className="btn btn-primary btn-info btn-sm" onClick={handleClick}>Followed</button>} */}
-                        { !isProfile && !follow && !self && <button className="btn btn-primary btn-info btn-sm" onClick={handleClick}>Follow</button>}
-                        {!isProfile && follow && !self && <button className="btn btn-primary btn-info btn-sm" onClick={handleClick}>Followed</button>}
-                    </div>
-                </div>
-            </div>
-        </div>
 
-    );
-};
+  return (
+    <div className="mb-4">
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+            {user && user.avatar === "" && <a className="image-link" href={`/profile/${user.id}`}><img className={classes.img} alt="complex" src="https://bootdey.com/img/Content/avatar/avatar1.png" /></a>}
+            {user && user.avatar !== "" && <a className="image-link" href={`/profile/${user.id}`}><img className={classes.img} alt="complex" src={user.avatar} /></a>}
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                <a href={`/profile/${user.id}`}>{user.firstName} {user.lastName}</a>
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {user.location}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {user.note}
+                </Typography>
+              </Grid>
+              {/* <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                  Remove
+                </Typography>
+              </Grid> */}
+            </Grid>
+            <Grid item>
+            {/* <IconButton aria-label="add to favorites" onClick={handleClick}> */}
+            { !isProfile && !follow && !self && <Button variant="contained" color="primary" aria-label="add to follow" onClick={handleClick}>Follow</Button>}
+            { !isProfile && follow && !self && <Button variant="contained" color="primary" aria-label="add to follow" onClick={handleClick}>Followed</Button>}
 
+
+            {/* <SendIcon /> */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+    </div>
+  );
+}
 export default UserGrid;
